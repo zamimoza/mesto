f0d903341dc7c37b51d4d26766dfe2f217b97258
albatrozs <vorontsovaz@gmail.com>
2022-01-23 23:55:40 +0300

    first review comments fixed

--- script/script.js
+++ script/script.js
@@ -1,28 +1,25 @@
 let popup = document.querySelector('.popup');
 let popupEditButton = document.querySelector('.profile__edit-button');
-let popupCloseButton = document.querySelector('.popup__cross');
-let popupSaveButton = document.querySelector('.popup__button');
+let popupCloseButton = document.querySelector('.popup__close-button');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__job');
+let nameInput = document.querySelector('.popup__input_name');
+let jobInput = document.querySelector('.popup__input_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
 
 function popupToggle() {
     if (!popup.classList.contains('popup_opened')) {
-        popup.classList.toggle('popup_opened');
         nameInput.value = profileName.textContent;
         jobInput.value = profileJob.textContent;
-    } else {
-        popup.classList.remove('popup_opened');
     }
+    popup.classList.toggle('popup_opened');
 }
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove('popup_opened');
+    popupToggle();
 }
 
 popupEditButton.addEventListener('click', popupToggle);
